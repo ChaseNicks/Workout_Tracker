83288eb24a2109882daccf8456ec995ecc4c4f1d
Chase Nicks <chasenicks81@gmail.com>
2021-10-12 20:46:24 -0400

    added models and built out exercises and the index file, still havent tested

---
+++ models/exercises.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now,
+        },
+        exercises: [
+            {
+                name: {
+                    type: String,
+                    trim: true,
+                    reuquired: "Please enter the name of your exercise!",
+                },
+                type: {
+                    type: String,
+                    trim: true,
+                    reuquired: "Please enter your exercise type!",
+                },
+                distance: {
+                    type: Number,
+                },
+                duration: {
+                    type: Number,
+                },
+                weight: {
+                    type: Number,
+                },
+                sets: {
+                    type: Number,
+                },
+                reps: {
+                    type: Number,
+                },
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    },
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercise.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("workout", WorkoutSchema);
+
+module.exports = Workout;
