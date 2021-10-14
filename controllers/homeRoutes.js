f25d4079dbd6bd82efc94afe092d651d752667a8
Chase Nicks <chasenicks81@gmail.com>
2021-10-14 15:42:19 -0400

    building out routes, will test later

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const path = require('path');
+
+router.get("/", ({res}) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+router.get("/exercise", ({res}) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
+});
+
+router.get("/stats", ({res}) => {
+    res.sendFile(path.join(__dirname, "../stats.html"))
+});
+
+module.export = router;
+
